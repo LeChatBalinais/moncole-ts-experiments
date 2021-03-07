cb81f9ccf3d42e22bc3ef01aec3202e8aae3e774
Unknown <chat.balinais@gmail.com>
2021-03-07 14:40:09 +0300

    Add first experiment

---
+++ src/app.ts
@@ -0,0 +1,62 @@
+import { Lens } from 'monocle-ts';
+
+interface Street {
+  num: number;
+  name: string;
+}
+interface Address {
+  city: string;
+  street: Street;
+}
+interface Company {
+  name: string;
+  address: Address;
+}
+interface Employee {
+  name: string;
+  company: Company;
+}
+
+const employee: Employee = {
+  name: 'john',
+  company: {
+    name: 'awesome inc',
+    address: {
+      city: 'london',
+      street: {
+        num: 23,
+        name: 'high street',
+      },
+    },
+  },
+};
+
+const capitalize = (s: string): string =>
+  s.substring(0, 1).toUpperCase() + s.substring(1);
+
+const employeeCapitalized = {
+  ...employee,
+  company: {
+    ...employee.company,
+    address: {
+      ...employee.company.address,
+      street: {
+        ...employee.company.address.street,
+        name: capitalize(employee.company.address.street.name),
+      },
+    },
+  },
+};
+
+const company = Lens.fromProp<Employee>()('company');
+const address = Lens.fromProp<Company>()('address');
+const street = Lens.fromProp<Address>()('street');
+const name = Lens.fromProp<Street>()('name');
+
+const capitalizeName = company
+  .compose(address)
+  .compose(street)
+  .compose(name)
+  .modify(capitalize);
+
+console.log(capitalizeName(employee), employeeCapitalized);
